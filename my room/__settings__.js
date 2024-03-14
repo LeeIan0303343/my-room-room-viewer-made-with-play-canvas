036b707477fd500e0ebcdd15e96f77dfc2668765
Ian <147879177+LeeIan0303343@users.noreply.github.com>
2024-03-14 13:09:19 -0400

    Add files via upload

---
+++ my room/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1879452.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'preferWebGpu': false,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 153648273, 153648265 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
